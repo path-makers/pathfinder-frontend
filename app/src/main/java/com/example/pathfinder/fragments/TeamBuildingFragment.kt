7fcc5f797d5ebc41c1a62b40a44d5c1e70569617
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2023-09-24 22:22:54 +0900

    팀 빌딩 페이지 추가

---
+++ app/src/main/java/com/example/pathfinder/fragments/TeamBuildingFragment.kt
@@ -0,0 +1,19 @@
+package com.example.pathfinder.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pathfinder.R
+
+class TeamBuildingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_team_building, container, false)
+    }
+}
