13cbc6fef6d73444d3130c83c635d7c6d72933a2
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2023-10-04 18:19:51 +0900

    코드정리

--- app/src/main/java/com/example/pathfinder/fragments/BoardFragment.kt
+++ app/src/main/java/com/example/pathfinder/fragments/BoardFragment.kt
@@ -30,20 +30,12 @@ class BoardFragment : Fragment() {
 
     private lateinit var boardRVAdapter: BoardListLVAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_board, container, false)
-
-
-
 
         boardRVAdapter = BoardListLVAdapter(boardDataList)
         binding.boardListView.adapter = boardRVAdapter
@@ -52,16 +44,6 @@ class BoardFragment : Fragment() {
 
         binding.boardListView.setOnItemClickListener { parent, view, position, id ->
 
-            //첫번쨰 방법으로는 listview에 있는 데이터 title content time 다 다른 액티비티로
-            //전달해줘서 만들기
-//            val intent = Intent(context,BoardInsideActivity::class.java)
-//            intent.putExtra("title",boardDataList[position].title)
-//            intent.putExtra("content",boardDataList[position].content)
-//            intent.putExtra("time",boardDataList[position].time)
-//            startActivity(intent)
-
-            //두번째 방법으로는 firebase에 있는 board에 대한 데이터의 id를 기반으로 다시 데이터를
-            //받아오는 방법
             val intent = Intent(context, BoardInsideView::class.java)
             intent.putExtra("key", boardKeyList[position])
             startActivity(intent)
@@ -89,8 +71,6 @@ class BoardFragment : Fragment() {
 
                 for (dataModel in snapshot.children) {
                     Log.d(TAG, dataModel.toString())
-//                    dataModel.key
-
                     val item = dataModel.getValue(BoardModel::class.java)
                     boardDataList.add(item!!)
                     boardKeyList.add(dataModel.key.toString())
