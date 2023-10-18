3d197be7e05c122dabb38d3f55497a4d1a8bfe3f
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2023-10-18 18:55:11 +0900

    게시판 글 작성 태그기능 구현

--- app/src/main/java/com/example/pathfinder/ui/board/view/BoardWriteActivity.kt
+++ app/src/main/java/com/example/pathfinder/ui/board/view/BoardWriteActivity.kt
@@ -7,29 +7,43 @@ import com.example.pathfinder.R
 import com.example.pathfinder.databinding.ActivityBoardWriteBinding
 import com.example.pathfinder.data.models.Board
 import com.example.pathfinder.data.repository.BoardRepository
+import com.example.pathfinder.ui.components.BottomSheetTagFragment
 import com.example.pathfinder.utils.FBAuth
 
 class BoardWriteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBoardWriteBinding
     private val repository = BoardRepository(this)
+    private val selectedTags = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
 
         initBoardWriteButton()
+        initTagButton()
     }
 
+
+    private fun initTagButton() {
+        binding.addButton.setOnClickListener {
+            val bottomSheet = BottomSheetTagFragment { tags ->
+                selectedTags.clear()
+                selectedTags.addAll(tags)
+                // TODO: 태그들 화면에 보이게 추가해야함
+            }
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
+    }
     private fun initBoardWriteButton() {
         binding.writeBtn.setOnClickListener {
             val title = binding.titleArea.text.toString()
             val content = binding.contentArea.text.toString()
             val uid = FBAuth.getUid()
             val boardType = "MENTEE"
-            val tags = listOf("IT", "TEST")
 
-            val board = Board(title, content, uid, "", boardType, tags)
+
+            val board = Board(title, content, uid, "", boardType, selectedTags)
             repository.sendBoardData(board)
 
             finish()
---
+++ app/src/main/java/com/example/pathfinder/ui/components/BottomSheetTagFragment.kt
@@ -0,0 +1,51 @@
+package com.example.pathfinder.ui.components
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.pathfinder.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetTagFragment(private val tagListener: (List<String>) -> Unit) : BottomSheetDialogFragment() {
+
+    private val selectedTags = mutableListOf<String>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottom_sheet_tag, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnConfirm = view.findViewById<Button>(R.id.btnConfirm)
+        val tagsButtons = listOf(
+            view.findViewById<Button>(R.id.tagItem1),
+            view.findViewById<Button>(R.id.tagItem2),
+            view.findViewById<Button>(R.id.tagItem3),
+            view.findViewById<Button>(R.id.tagItem4)
+        )
+
+        tagsButtons.forEach { tagButton ->
+            tagButton.setOnClickListener {
+                toggleTagSelection(tagButton.text.toString(), tagButton)
+            }
+        }
+
+        btnConfirm.setOnClickListener {
+            tagListener(selectedTags)
+            dismiss()
+        }
+    }
+
+    private fun toggleTagSelection(tag: String, tagButton: Button) {
+        if (selectedTags.contains(tag)) {
+            selectedTags.remove(tag)
+            tagButton.isSelected = false
+        } else {
+            selectedTags.add(tag)
+            tagButton.isSelected = true
+        }
+    }
+}
