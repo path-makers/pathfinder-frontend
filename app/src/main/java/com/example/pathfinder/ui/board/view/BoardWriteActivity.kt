790a8adec66331e319c0974eb15e5579e691e6fc
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2023-10-27 18:43:34 +0900

    태그 선택시 화면에 보이게 구현

--- app/src/main/java/com/example/pathfinder/ui/board/view/BoardWriteActivity.kt
+++ app/src/main/java/com/example/pathfinder/ui/board/view/BoardWriteActivity.kt
@@ -2,9 +2,11 @@ package com.example.pathfinder.ui.board.view
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -57,10 +59,28 @@ class BoardWriteActivity : AppCompatActivity() {
                 selectedTags.clear()
                 selectedTags.addAll(tags)
                 // TODO: 태그들 화면에 보이게 추가해야함
+                displaySelectedTags()
             }
             bottomSheet.show(supportFragmentManager, bottomSheet.tag)
         }
     }
+
+
+
+    private fun displaySelectedTags() {
+        // 기존에 추가된 태그 뷰를 모두 제거합니다.
+        binding.tagsLayout.removeAllViews()
+
+        // 선택된 태그를 순회하며 화면에 추가합니다.
+        for (tag in selectedTags) {
+            val tagView = LayoutInflater.from(this).inflate(R.layout.item_tag, binding.tagsLayout, false)
+            val tagName = tagView.findViewById<TextView>(R.id.tagName)
+            tagName.text = tag
+            binding.tagsLayout.addView(tagView)
+        }
+    }
+
+
     private fun initBoardWriteButton() {
         binding.writeBtn.setOnClickListener {
             val title = binding.titleArea.text.toString()
