70c11e62da668b0ed8331a87ab428a729dd2c9ac
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2023-10-31 19:08:12 +0900

    게시판 태그 보이게 구현

--- app/src/main/java/com/example/pathfinder/ui/board/view/BoardListLVAdapter.kt
+++ app/src/main/java/com/example/pathfinder/ui/board/view/BoardListLVAdapter.kt
@@ -31,14 +31,16 @@ class BoardListLVAdapter(val boardList : MutableList<Board>): BaseAdapter(){
         var view = convertView
         view = LayoutInflater.from(parent?.context).inflate(R.layout.board_list_item, parent, false)
 
-        val itemLinearLayoutView = view?.findViewById<LinearLayout>(R.id.itemView)
 
+        val tagsLayout = view.findViewById<LinearLayout>(R.id.tagsLayout)
         val title = view?.findViewById<TextView>(R.id.titleArea)
         val content = view?.findViewById<TextView>(R.id.contentArea)
         val date = view?.findViewById<TextView>(R.id.timeArea)  // 이름을 time에서 date로 변경
         val uid = view?.findViewById<TextView>(R.id.userName)    // 이름을 userName에서 uid로 변경
 
-
+        if (tagsLayout != null && boardList[position].tags.isNotEmpty()) {
+            displayTags(tagsLayout, boardList[position].tags)
+        }
         title!!.text = boardList[position].title
         content!!.text = boardList[position].content
         date!!.text = formatDate(boardList[position].date.toLong())
@@ -53,4 +55,15 @@ class BoardListLVAdapter(val boardList : MutableList<Board>): BaseAdapter(){
         return sdf.format(date)
     }
 
+    private fun displayTags(tagsLayout: LinearLayout, tags: List<String>) {
+        tagsLayout.removeAllViews() // 기존에 추가된 모든 뷰를 제거
+        for (tag in tags) {
+            // inflate 메서드로 뷰를 생성
+            val tagViewLayout = LayoutInflater.from(tagsLayout.context).inflate(R.layout.item_tag, tagsLayout, false)
+            // tagName ID를 가진 TextView를 찾음
+            val tagTextView = tagViewLayout.findViewById<TextView>(R.id.tagName)
+            tagTextView.text = tag // TextView에 태그 텍스트를 설정
+            tagsLayout.addView(tagViewLayout) // LinearLayout에 루트 레이아웃을 추가
+        }
+    }
 }
