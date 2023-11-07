7a102177618270547edaaeade63b267018e1e675
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2023-11-07 18:32:52 +0900

    캐싱 롤백

--- app/src/main/java/com/example/pathfinder/ui/board/view/BoardFragment.kt
+++ app/src/main/java/com/example/pathfinder/ui/board/view/BoardFragment.kt
@@ -11,7 +11,6 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pathfinder.R
@@ -26,18 +25,13 @@ import java.io.Serializable
 class BoardFragment : Fragment() {
     private lateinit var viewModel: BoardViewModel
     private lateinit var binding: FragmentBoardBinding
-    private val mentorBoardDataList = mutableListOf<Board>()
-    private val menteeBoardDataList = mutableListOf<Board>()
-    private var mentorBoardObserver: Observer<List<Board>>? = null
-    private var menteeBoardObserver: Observer<List<Board>>? = null
-    private var currentBoardType = "MENTOR"
 
     private val boardDataList = mutableListOf<Board>()
     private lateinit var boardRVAdapter: BoardRecyclerViewAdapter
 
     private val startWriteActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            getFBBoardData(currentBoardType)
+            getFBBoardData("MENTOR")
         }
     }
 
@@ -67,49 +61,28 @@ class BoardFragment : Fragment() {
         binding.boardListView.layoutManager = LinearLayoutManager(context)
 
         binding.mentorBtn.setOnClickListener {
-            switchBoardType("MENTOR")
+
+            binding.mentorBtn.isChecked = true
+            binding.menteeBtn.isChecked = false
+
+            binding.mentorBtn.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            binding.menteeBtn.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray))
+
+            getFBBoardData("MENTOR")
         }
 
         binding.menteeBtn.setOnClickListener {
-            switchBoardType("MENTEE")
+
+            binding.menteeBtn.isChecked = true
+            binding.mentorBtn.isChecked = false
+
+            binding.menteeBtn.setTextColor(ContextCompat.getColor(requireContext(),  R.color.black))
+            binding.mentorBtn.setTextColor(ContextCompat.getColor(requireContext(),  R.color.gray))
+
+            getFBBoardData("MENTEE")
         }
 
 
-    }
-
-    private fun switchBoardType(boardType: String) {
-        if (currentBoardType != boardType) {
-            currentBoardType = boardType
-            when (boardType) {
-                "MENTOR" -> {
-                    updateUI(mentorBoardDataList)
-                }
-                "MENTEE" -> {
-                    if (menteeBoardDataList.isEmpty()) {
-                        getFBBoardData("MENTEE")
-                    } else {
-                        updateUI(menteeBoardDataList)
-                    }
-                }
-            }
-        }
-
-        updateButtonStyles(boardType)
-    }
-
-    private fun updateUI(boardList: List<Board>) {
-        boardDataList.clear()
-        boardDataList.addAll(boardList)
-        boardRVAdapter.notifyDataSetChanged()
-    }
-
-    private fun updateButtonStyles(boardType: String) {
-        val isMentor = boardType == "MENTOR"
-        binding.mentorBtn.isChecked = isMentor
-        binding.menteeBtn.isChecked = !isMentor
-
-        binding.mentorBtn.setTextColor(ContextCompat.getColor(requireContext(), if (isMentor) R.color.black else R.color.gray))
-        binding.menteeBtn.setTextColor(ContextCompat.getColor(requireContext(), if (isMentor) R.color.gray else R.color.black))
     }
 
     private fun initWriteButton() {
@@ -122,30 +95,17 @@ class BoardFragment : Fragment() {
 
 
     private fun getFBBoardData(boardType: String) {
-        // 관찰자를 먼저 삭제
-        mentorBoardObserver?.let { viewModel.boardDataList.removeObserver(it) }
-        menteeBoardObserver?.let { viewModel.boardDataList.removeObserver(it) }
         viewModel.getBoardData(boardType)
-        // 새 관찰자를 생성
-        val boardObserver = Observer<List<Board>> { newData ->
-            val targetList = if (boardType == "MENTOR") mentorBoardDataList else menteeBoardDataList
-            targetList.clear()
-            targetList.addAll(newData.reversed())
-            if (currentBoardType == boardType) {
-                // 현재 보고 있는 타입이면 UI를 업데이트
-                updateUI(targetList)
-            }
+        viewModel.boardDataList.observe(viewLifecycleOwner) { boardDataList ->
+            this.boardDataList.clear()
+            this.boardDataList.addAll(boardDataList)
+            this.boardDataList.reverse()
+            boardRVAdapter.notifyDataSetChanged()
         }
 
-        // 관찰자를 적절한 변수에 저장
-        if (boardType == "MENTOR") {
-            mentorBoardObserver = boardObserver
-        } else {
-            menteeBoardObserver = boardObserver
+        viewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
+            // 에러 처리
         }
-
-        // LiveData 관찰자를 등록
-        viewModel.boardDataList.observe(viewLifecycleOwner, boardObserver)
     }
 
 }
