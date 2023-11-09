1883246816d51d46d76ac0ccc5a861a1186226e7
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2023-11-09 23:35:39 +0900

    코멘트RV어댑터 작성

---
+++ app/src/main/java/com/example/pathfinder/utils/CommentRVAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.pathfinder.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pathfinder.R
+import com.example.pathfinder.data.models.Comment
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class CommentRVAdapter(val commentList : MutableList<Comment>) : RecyclerView.Adapter<CommentRVAdapter.CommentViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.comment_list_item,parent,false)
+        return CommentViewHolder(view)
+    }
+
+    class CommentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val author:TextView = view.findViewById(R.id.userNameArea)
+        val content:TextView = view.findViewById(R.id.commentArea)
+        val date:TextView = view.findViewById(R.id.timeArea)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        val comment = commentList[position]
+        holder.author.text = comment.author
+        holder.content.text = comment.content
+        holder.date.text = formatDate(comment.createdAt.toLong())
+    }
+
+    override fun getItemCount(): Int {
+        return commentList.size
+    }
+
+
+
+    private fun formatDate(timeStamp: Long): String {
+        val date = Date(timeStamp)
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+        return sdf.format(date)
+    }
+}
