4f1b52f3f38270c8f74a15e81754b67a715ee469
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2023-11-25 19:01:43 +0900

    Dagger Hilt,Coroutine 환경설정

---
+++ app/src/main/java/com/example/pathfinder/domain/usecases/GetBoardDataMentorUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.pathfinder.domain.usecases
+
+import com.example.pathfinder.data.models.Board
+import com.example.pathfinder.data.repository.BoardRepository
+
+class GetBoardDataMentorUseCase(private val boardRepository: BoardRepository) {
+
+    //todo: 코루틴으로 바꾸기
+    //todo: 유즈케이스 리팩토링
+}
