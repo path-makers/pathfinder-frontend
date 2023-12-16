45c7989c6bbcb9b2acabdbf3199823e3366d3c47
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2023-12-16 23:20:31 +0900

    [Refactor] 홈 화면 로직 변경

--- app/src/main/java/com/example/pathfinder/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pathfinder/ui/home/HomeFragment.kt
@@ -8,25 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pathfinder.R
 import com.example.pathfinder.data.model.Board
-import com.example.pathfinder.data.repository.BoardRepository
+import com.example.pathfinder.data.model.Results
 import com.example.pathfinder.databinding.FragmentHomeBinding
-import com.example.pathfinder.ui.board.viewModel.BoardViewModel
-import com.example.pathfinder.ui.board.viewModel.BoardViewModelFactory
+import com.example.pathfinder.ui.board.viewModel.BoardRefactorViewModel
 import com.example.pathfinder.utils.FBAuth
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
 
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var viewModel: BoardViewModel
+    private val viewModel: BoardRefactorViewModel by viewModels()
     private lateinit var boardRVAdapterMentor: HomeRVAdapter
     private lateinit var boardRVAdapterMentee: HomeRVAdapter
     private lateinit var boardRVAdapterAlgorithm: HomeRVAlgorithmAdapter
@@ -35,12 +35,78 @@ class HomeFragment : Fragment() {
     private val boardDataListAlgorithm = mutableListOf<Board>()
 
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        initButtons()
+        initBoardRecyclerView()
+        hideBottomNavigation(false);
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getBoardDataByAlgorithm(FBAuth.getUid())
+
+        viewModel.boardDataListMentor.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Results.Loading -> {
+                    // 로딩 UI 처리
+                }
+
+                is Results.Success -> {
+                    val teamData = result.data
+                    // 성공적으로 데이터를 받았을 때 UI 업데이트
+                    updateMentor(teamData)
+                }
+
+                is Results.Failure -> {
+                    // 오류 UI 처리
+                }
+            }
+        }
+
+        viewModel.boardDataListMentee.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Results.Loading -> {
+                    // 로딩 UI 처리
+                }
+
+                is Results.Success -> {
+                    val teamData = result.data
+                    // 성공적으로 데이터를 받았을 때 UI 업데이트
+                    updateMentee(teamData)
+                }
+
+                is Results.Failure -> {
+                    // 오류 UI 처리
+                }
+            }
+        }
+
+        viewModel.boardDataListAlgorithm.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Results.Loading -> {
+                    // 로딩 UI 처리
+                }
+
+                is Results.Success -> {
+                    val teamData = result.data
+                    // 성공적으로 데이터를 받았을 때 UI 업데이트
+                    updateAlgorithm(teamData)
+                }
+
+                is Results.Failure -> {
+                    // 오류 UI 처리
+                }
+            }
+        }
+
+    }
+
+    private fun initButtons() {
         binding.chatButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_aiIntroFragment)
         }
@@ -56,29 +122,8 @@ class HomeFragment : Fragment() {
             (requireActivity().findViewById(R.id.bottomNavigationView) as BottomNavigationView)
                 .selectedItemId = R.id.boardFragment
         }
-
-        val boardRepository = BoardRepository(requireContext())
-        val viewModelFactory = BoardViewModelFactory(boardRepository)
-        viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[BoardViewModel::class.java]
-
-        initBoardRecyclerView()
-        hideBottomNavigation(false);
-
-
-        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        if (viewModel.boardDataListMentor.value == null) {
-            getBoardData()
-
-        }
-
-
-        subscribeToData()
-    }
 
     private fun initBoardRecyclerView() {
         boardRVAdapterMentor = HomeRVAdapter(boardDataListMentor)
@@ -89,41 +134,43 @@ class HomeFragment : Fragment() {
         binding.recommendList.adapter = boardRVAdapterAlgorithm
         binding.mentorList.layoutManager = LinearLayoutManager(context)
         binding.menteeList.layoutManager = LinearLayoutManager(context)
-        binding.recommendList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.recommendList.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
     }
 
-    private fun getBoardData() {
 
-        viewModel.getBoardDataMentor()
-        viewModel.getBoardDataMentee()
-        viewModel.getBoardDataByAlgorithm(FBAuth.getUid())
-    }
-
-    private fun subscribeToData() {
+    private fun updateMentor(teamData: List<Board>) {
 
         viewModel.boardDataListMentor.observe(viewLifecycleOwner) { boardDataListMentor ->
             this.boardDataListMentor.clear()
-            this.boardDataListMentor.addAll(boardDataListMentor)
-            this.boardDataListMentor.reverse()
+            this.boardDataListMentor.addAll(teamData)
             boardRVAdapterMentor.notifyDataSetChanged()
         }
+
+
+    }
+
+    private fun updateMentee(teamData: List<Board>) {
+
         viewModel.boardDataListMentee.observe(viewLifecycleOwner) { boardDataListMentee ->
             this.boardDataListMentee.clear()
-            this.boardDataListMentee.addAll(boardDataListMentee)
-            this.boardDataListMentee.reverse()
+            this.boardDataListMentee.addAll(teamData)
             boardRVAdapterMentee.notifyDataSetChanged()
-
         }
 
+
+    }
+
+    private fun updateAlgorithm(teamData: List<Board>) {
 
         viewModel.boardDataListAlgorithm.observe(viewLifecycleOwner) { boardDataListAlgorithm ->
             this.boardDataListAlgorithm.clear()
-            this.boardDataListAlgorithm.addAll(boardDataListAlgorithm)
+            this.boardDataListAlgorithm.addAll(teamData)
             this.boardDataListAlgorithm.reverse()
             boardRVAdapterAlgorithm.notifyDataSetChanged()
-
         }
+
 
     }
 
